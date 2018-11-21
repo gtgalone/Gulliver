f8b46c26e47152e37750209e4a99e745ab47f7f1
gtgalone <sjhgenius@gmail.com>
2018-11-20 19:08:07 -0500

    Test geocoder

--- app/src/main/java/com/example/jehun/gulliver/MainActivity.kt
+++ app/src/main/java/com/example/jehun/gulliver/MainActivity.kt
@@ -1,61 +1,191 @@
 package com.example.jehun.gulliver
 
-import android.content.Context
-import android.content.DialogInterface
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
-import android.location.LocationManager
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
-import android.support.v7.app.AlertDialog
+import android.support.v4.app.ActivityCompat
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlin.system.exitProcess
 
-class MainActivity : AppCompatActivity(), com.google.android.gms.location.LocationListener {
 
-    lateinit var locationManager : LocationManager
-    private var mLocationManager: LocationManager? = null
+class MainActivity : AppCompatActivity() {
 
-    override fun onLocationChanged(location: Location) {
-        val msg = "Updated Location: Latitude " + location.longitude.toString() + location.longitude
-        current.text = location.latitude.toString() + ":" + location.longitude
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-    }
+    /**
+     * Provides the entry point to the Fused Location Provider API.
+     */
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    /**
+     * Represents a geographical location.
+     */
+    protected var mLastLocation: Location? = null
+    private var mLatitudeLabel: String? = null
+    private var mLongitudeLabel: String? = null
+    private var mLatitudeText: TextView? = null
+    private var mLongitudeText: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mLocationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        mLatitudeLabel = resources.getString(R.string.latitude_label)
+        mLongitudeLabel = resources.getString(R.string.longitude_label)
+        mLatitudeText = findViewById<View>(R.id.latitude_text) as TextView
+        mLongitudeText = findViewById<View>(R.id.longitude_text) as TextView
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
-    private fun checkLocation(): Boolean {
-        if(!isLocationEnabled())
-            showAlert();
-        return isLocationEnabled()
+    public override fun onStart() {
+        super.onStart()
+        if (!checkPermissions()) {
+            requestPermissions()
+        } else {
+            getLastLocation()
+        }
+    }/**
+     * Provides a simple way of getting a device's location and is well suited for
+     * applications that do not require a fine-grained location and that do not need location
+     * updates. Gets the best and most recent location currently available, which may be null
+     * in rare cases when a location is not available.
+     *
+     *
+     * Note: this method should be called after location permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        mFusedLocationClient!!.lastLocation
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful && task.result != null) {
+                    mLastLocation = task.result
+                    mLatitudeText!!.setText(
+                        mLatitudeLabel+":   "+
+                                (mLastLocation )!!.latitude)
+                    mLongitudeText!!.setText(mLongitudeLabel+":   "+
+                            (mLastLocation )!!.longitude)
+
+                    val aa = Geocoder(this)
+                    println((mLastLocation)!!.latitude)
+                    println((mLastLocation)!!.longitude)
+                    println(aa.getFromLocation((mLastLocation)!!.latitude, (mLastLocation)!!.longitude, 10))
+                    println(aa.getFromLocationName("Syracuse", 10))
+                } else {
+                    Log.w(TAG, "getLastLocation:exception", task.exception)
+                    showMessage(getString(R.string.no_location_detected))
+                }
+            }
     }
-
-    private fun isLocationEnabled(): Boolean {
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    /**
+     * Shows a [] using `text`.
+     * @param text The Snackbar text.
+     */
+    private fun showMessage(text: String) {
+        val container = findViewById<View>(R.id.main_activity_container)
+        if (container != null) {
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+        }
     }
-
-
-    private fun showAlert() {
-        val dialog = AlertDialog.Builder(this)
-        dialog.setTitle("Enable Location")
-            .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " + "use this app")
-            .setPositiveButton("Location Settings", DialogInterface.OnClickListener { paramDialogInterface, paramInt ->
-                val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(myIntent)
-            })
-            .setNegativeButton("Cancel", DialogInterface.OnClickListener { paramDialogInterface, paramInt -> })
-        dialog.show()
+    /**
+     * Shows a [].
+     * @param mainTextStringId The id for the string resource for the Snackbar text.
+     * *
+     * @param actionStringId   The text of the action item.
+     * *
+     * @param listener         The listener associated with the Snackbar action.
+     */
+    private fun showSnackbar(mainTextStringId: Int, actionStringId: Int,
+                             listener: View.OnClickListener) {
+        Toast.makeText(this@MainActivity, getString(mainTextStringId), Toast.LENGTH_LONG).show()
+    }
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(this@MainActivity,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE)
+    }
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION)
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                View.OnClickListener {
+                    // Request permission
+                    startLocationPermissionRequest()
+                })
+        } else {
+            Log.i(TAG, "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            startLocationPermissionRequest()
+        }
+    }
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getLastLocation()
+            } else {
+                // Permission denied.
+                // Notify the user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                    View.OnClickListener {
+                        // Build intent that displays the App settings screen.
+                        val intent = Intent()
+                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        val uri = Uri.fromParts("package",
+                            BuildConfig.APPLICATION_ID, null)
+                        intent.data = uri
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    })
+            }
+        }
     }
 
     fun close(v: View) {
         moveTaskToBack(true)
         exitProcess(-1)
+    }
+
+    companion object {
+        private val TAG = "LocationProvider"
+        private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
     }
 }
